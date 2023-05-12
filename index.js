0d79f0c85643c9abeea8aae34687ad83577b094a
Kang song <132147692+sampleRID@users.noreply.github.com>
2023-05-12 10:17:20 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+// javascript
+
+let countOne = document.getElementById("zero1")
+console.log(countOne)
+
+let count = 0
+function buttonOne() {
+    count = count + 1
+    countOne.innerText = count
+}
+
+let countTwo =  document.getElementById("zero1")
+console.log(countTwo)
+
+function buttonTwo() {
+    count = count + 2
+    countTwo.innerText = count
+}
+
+let countThree =  document.getElementById("zero1")
+console.log(countThree)
+
+function buttonThree() {
+    count = count + 3
+    countThree.innerText = count
+}
+
+
+
+let countA = document.getElementById("zero-2")
+console.log(countA)
+
+let counti1 = 0
+function buttonA() {
+    counti1 = counti1 + 1
+    countA.innerText = counti1
+}
+
+let countB =  document.getElementById("zero-2")
+console.log(countB)
+
+function buttonB() {
+    counti1 = counti1 + 2
+    countB.innerText = counti1
+}
+
+let countC =  document.getElementById("zero-2")
+console.log(countC)
+
+function buttonC() {
+    counti1 = counti1 + 3
+    countC.innerText = counti1
+}
